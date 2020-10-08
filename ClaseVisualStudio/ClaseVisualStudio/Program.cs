c02c7ac50ebb0655b9d405b70671e3af2079b573
jmarinprofe <jmarinprofe@gmail.com>
2020-10-08 14:43:48 +0200

    Test commit
    Commit de prueba

--- ClaseVisualStudio/ClaseVisualStudio/Program.cs
+++ ClaseVisualStudio/ClaseVisualStudio/Program.cs
@@ -6,7 +6,7 @@ namespace ClaseVisualStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(";) ;) ;)");
+            Console.WriteLine(";) ;) ;) ");
         }
     }
 }
